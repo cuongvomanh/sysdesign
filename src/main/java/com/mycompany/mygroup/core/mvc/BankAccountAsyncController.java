2fc08b31333991da8796f78dff85c324896e9ca0
cuong <cuongvm12>
2020-07-23 12:17:07 +0700

    run ok with async controller

---
+++ src/main/java/com/mycompany/mygroup/core/mvc/BankAccountAsyncController.java
@@ -0,0 +1,34 @@
+package com.mycompany.mygroup.core.mvc;
+
+import com.mycompany.mygroup.core.entity.BankAccount;
+import com.mycompany.mygroup.core.infra.di.DependencyResolver;
+import com.mycompany.mygroup.core.usecase.RequestModel;
+import com.mycompany.mygroup.core.usecase.ResponseModel;
+import com.mycompany.mygroup.core.usecase.account.BankAccountBoundary;
+
+import javax.management.InstanceNotFoundException;
+import java.math.BigDecimal;
+
+public class BankAccountAsyncController implements IBankAccountController {
+    private BankAccountBoundary bankAccountBoundary;
+
+    public BankAccountAsyncController() throws InstanceNotFoundException {
+        this.bankAccountBoundary = (BankAccountBoundary) DependencyResolver.getInstance("BankAccountBoundary");
+    }
+
+    public ResponseModel deposit(String accountNumber, BigDecimal amount) {
+        RequestModel requestModel = new RequestModel(accountNumber, amount);
+        new Thread(() -> {
+            this.bankAccountBoundary.deposit(requestModel);
+        }).start();
+        return new ResponseModel("200");
+    }
+
+    public ResponseModel withdraw(String accountNumber, BigDecimal amount) {
+        RequestModel requestModel = new RequestModel(accountNumber, amount);
+        new Thread(() -> {
+            this.bankAccountBoundary.withdraw(requestModel);
+        }).start();
+        return new ResponseModel("200");
+    }
+}
--- src/main/java/com/mycompany/mygroup/core/usecase/ResponseModel.java
+++ src/main/java/com/mycompany/mygroup/core/usecase/ResponseModel.java
@@ -3,6 +3,13 @@ package com.mycompany.mygroup.core.usecase;
 public class ResponseModel {
     private String result;
 
+    public ResponseModel() {
+    }
+
+    public ResponseModel(String result) {
+        this.result = result;
+    }
+
     public String getResult() {
         return result;
     }
